2b2aa614c094f890c1fb363f58edd51e2f961e29
ShonSagoro <ramosproque@gmail.com>
2024-02-18 18:45:49 -0600

    add database

--- src/UserManagement/Infraestructure/Routes/UserRoutes.ts
+++ src/UserManagement/Infraestructure/Routes/UserRoutes.ts
@@ -1,15 +1,20 @@
 import { Express } from "express";
 import JWTMiddleware from "../../../Middleware/JWTMiddlewre";
 import { activateUserController, deleteUserController, getByUuidController, listUsersController, singInUserController, singOutUserCase, singOutUserController, singUpUserController, updateUserController } from "../dependecies";
+import dotenv from 'dotenv';
+dotenv.config();
 
 const Verifytoken = JWTMiddleware.VerifyToken;
+const MODEL_URL = "users/";
+const BASE_URL = process.env.BASE_URL || "/api/v1/";
+
 export function setupUserRoutes(app: Express) {
-    app.post('/users/sing_up', singUpUserController.execute.bind(singUpUserController));
-    app.post('/users/sing_in', singInUserController.execute.bind(singInUserController));
-    app.get('/users/:uuid',Verifytoken, getByUuidController.execute.bind(getByUuidController));
-    app.put('/users/:uuid',Verifytoken, updateUserController.execute.bind(updateUserController));
-    app.delete('/users/:uuid',Verifytoken, deleteUserController.execute.bind(deleteUserController));
-    app.get('/users/activate/:uuid', activateUserController.execute.bind(activateUserController));
-    app.get('/users/', listUsersController.execute.bind(listUsersController));
-    app.get('/users/sing_out/:uuid', Verifytoken, singOutUserController.execute.bind(singOutUserController));
+    app.post(`${BASE_URL}${MODEL_URL}sing_up`, singUpUserController.execute.bind(singUpUserController));
+    app.post(`${BASE_URL}${MODEL_URL}sing_in`, singInUserController.execute.bind(singInUserController));
+    app.get(`${BASE_URL}${MODEL_URL}:uuid`, Verifytoken, getByUuidController.execute.bind(getByUuidController));
+    app.put(`${BASE_URL}${MODEL_URL}:uuid`, Verifytoken, updateUserController.execute.bind(updateUserController));
+    app.delete(`${BASE_URL}${MODEL_URL}:uuid`, Verifytoken, deleteUserController.execute.bind(deleteUserController));
+    app.get(`${BASE_URL}${MODEL_URL}activate/:uuid`, activateUserController.execute.bind(activateUserController));
+    app.get(`${BASE_URL}${MODEL_URL}`, listUsersController.execute.bind(listUsersController));
+    app.get(`${BASE_URL}${MODEL_URL}sing_out/:uuid`, Verifytoken, singOutUserController.execute.bind(singOutUserController));
 }
