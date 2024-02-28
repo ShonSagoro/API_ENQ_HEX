060f65ca1437d44fde86b9466fe554a7b320710a
ShonSagoro <ramosproque@gmail.com>
2024-02-27 23:49:12 -0600

    fix image service

--- src/HotelManagement/Infraestructure/Routes/HotelRoutes.ts
+++ src/HotelManagement/Infraestructure/Routes/HotelRoutes.ts
@@ -8,15 +8,7 @@ dotenv.config();
 const Verifytoken = JWTMiddleware.VerifyTokenTry;
 const MODEL_URL = "hotels";
 const BASE_URL = process.env.BASE_URL || "/api/v1/";
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, 'public/images');
-    },
-    filename: (req, file, cb) => {
-      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-      cb(null, file.fieldname + '-' + uniqueSuffix + '.' + file.originalname.split('.').pop());
-    }
-  });
+const storage = multer.diskStorage({});
 const upload = multer({ storage });
 
 export function setUpHotelRoutes(app: Express) {
