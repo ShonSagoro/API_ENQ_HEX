9f81cff1f19b3c84e80119fdba6291a113fe90a6
ShonSagoro <ramosproque@gmail.com>
2024-03-01 00:46:57 -0600

    add routes

--- src/ReservationsManagement/Infrastructure/Routes/ReservationRoutes.ts
+++ src/ReservationsManagement/Infrastructure/Routes/ReservationRoutes.ts
@@ -8,8 +8,8 @@ const Verifytoken = JWTMiddleware.VerifyTokenTry;
 const MODEL_URL = "reservatios";
 const BASE_URL = process.env.BASE_URL || "/api/v1/";
 
-export function setupUserRoutes(app: Express) {
-    app.post(`${BASE_URL}${MODEL_URL}/sing_up`, Verifytoken, createReservationController.execute.bind(createReservationController));
+export function setupReservationRoutes(app: Express) {
+    app.post(`${BASE_URL}${MODEL_URL}/`, Verifytoken, createReservationController.execute.bind(createReservationController));
     app.get(`${BASE_URL}${MODEL_URL}/user/:uuid`, Verifytoken, findAllByUUIDUserReservationController.execute.bind(findAllByUUIDUserReservationController));
     app.get(`${BASE_URL}${MODEL_URL}/:uuid`, Verifytoken, findByUUIDReservationController.execute.bind(findByUUIDReservationController));
     app.delete(`${BASE_URL}${MODEL_URL}/:uuid`, Verifytoken, deleteReservationController.execute.bind(deleteReservationController));
